ea67feaa0ffbbfebcd93a7ad92d5b70ddea12c57
AracelyC <aracely.campa233215@potros.itson.edu.mx>
2025-04-19 02:32:08 -0700

    Eliminacion total de codigo
    Se elimino todo el codigo front para hacerse de cero

--- sistema_fotografia/src/DTO/create-bundle.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateBundleDTO{
-    name: string
-    price: number
-    url: string
-    contractsId: number[]
-    itemsID: number[]
-    eventsID:number[]
-    appointmentTemplateID:number
-}
--- sistema_fotografia/src/DTO/report-appointment.dto.ts
+++
@@ -1,5 +0,0 @@
-export class ReportAppointmentDTO{
-    startDate:Date
-    endDate:Date
-    bundleId: number
-}
